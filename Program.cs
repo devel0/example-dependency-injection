0a87cfa84ca721cbf0e0121fa7368c2c2a80ea80
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-29 12:27:02 +0200

    DI benchmark

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
-﻿using example_di;
+﻿using System.Diagnostics;
+using example_di;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+const int A = 0xffff;
+const int B = 0x2fff;
+
+var sw_non_di = new Stopwatch();
+{    
+    sw_non_di.Start();
+    var sum = 0d;
+    for (int i = 0; i < A; ++i)
+    {
+        for (int j = 0; j < B; ++j)
+        {
+            sum = new Service1().Sum(i, j);
+        }
+    }
+    sw_non_di.Stop();    
+}
 
 var builder = Host.CreateDefaultBuilder(args);
 
@@ -13,4 +31,22 @@ builder.ConfigureServices(services => services
 
 using var host = builder.Build();
 
-host.Run();
+var sw_di = new Stopwatch();
+{    
+    sw_di.Start();
+    var sum = 0d;
+    for (int i = 0; i < A; ++i)
+    {
+        for (int j = 0; j < B; ++j)
+        {
+            sum = host.Services.GetRequiredService<IService1>().Sum(i, j);
+        }
+    }
+    sw_di.Stop();    
+}
+
+System.Console.WriteLine($"DI BENCHMARK");
+System.Console.WriteLine($"  without : {sw_non_di.Elapsed}");
+System.Console.WriteLine($"  with    : {sw_di.Elapsed}");
+
+// host.Run();
